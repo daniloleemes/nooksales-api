8c1c9b7e97fb656f334b184eeccd40e87de2274a
Danilo Lemes <daniloleemes@hotmail.com>
2020-04-17 18:40:36 +0100

    fix lint

--- src/collectibles/collectibles.resolver.ts
+++ src/collectibles/collectibles.resolver.ts
@@ -1,18 +1,13 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { CollectiblesService } from './collectibles.service';
 import { CollectibleType } from './models/collectible-type.model';
-import { UseGuards } from '@nestjs/common';
-import { GqlAuthGuard } from 'src/auth/gql-auth.guard';
-import { CurrentUser } from 'src/users/user.decorator';
-import { User } from 'src/users/interface/user.interface';
 
 @Resolver('Collectibles')
 export class CollectiblesResolver {
     constructor(private readonly collectiblesService: CollectiblesService) {}
 
     @Query(() => [CollectibleType])
-    @UseGuards(GqlAuthGuard)
-    async collectibles(@CurrentUser() user: User): Promise<CollectibleType[]> {
+    async collectibles(): Promise<CollectibleType[]> {
         return this.collectiblesService.findAll();
     }
 
