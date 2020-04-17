d86d3cc1cd75950e277fe30a4c55d1ac7c117bda
Danilo Lemes <daniloleemes@hotmail.com>
2020-04-17 23:51:13 +0100

    fix inheritance

--- src/listings/interface/listing.interface.ts
+++ src/listings/interface/listing.interface.ts
@@ -1,6 +1,6 @@
 import { Document } from 'mongoose';
 
-export interface Listing {
+export interface Listing extends Document {
     readonly userId: string;
     readonly itemId: string;
     readonly sellOnly: boolean;
