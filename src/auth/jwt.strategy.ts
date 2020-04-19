d05babb52a36bcc63c13ef8f805cbaf32aad6442
Danilo Lemes <daniloleemes@hotmail.com>
2020-04-19 22:25:23 +0100

    ignore jwt expiration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private readonly usersService: UsersService) {
         super({
             jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-            ignoreExpiration: false,
+            ignoreExpiration: true,
             secretOrKey: 'nooksales',
         })
     }
